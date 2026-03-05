841de0ba0b24c66a91d6fae01cafd51b46fc0a5d
EwanG <E.Glass2758@student.leedsbeckett.ac.uk>
2026-03-05 16:04:57 +0000

    CHECKPOINT - flask app login, Javascript request:
    - The flask app has been implemented and will be the main server for this project. It has a login function that takes in the username and password. This application puts the data into a json file format.
    
    - The Javascript file has also been updated with a fetch function where javascript sends in the request for the login.
    
    - I also did some minor touch ups with the html and css files though these are very minor (such as fonts and class names).

---
+++ src/Python Backend (Flask)/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+'''This is the server for the application.
+This server allows us to be able to add the functions for each
+of the html pages and test them to make sure that they work
+using the flask web framework'''
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    data = request.json
+    username = data['username']
+    password = data['password']
+
+    if username == "raspberry" and password == "team17":
+        return jsonify({"status": "success"})
+    else:
+        return jsonify({"status": "fail"})
+
+app.run(host="0.0.0.0", port=5000)
