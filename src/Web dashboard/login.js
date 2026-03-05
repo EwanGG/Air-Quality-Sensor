428867b393d9a2cbbb6ee0ab72ca83cf43925586
EwanG <E.Glass2758@student.leedsbeckett.ac.uk>
2026-03-05 20:26:19 +0000

    CHECKPOINT - HTML and CSS stylesheet:
    - I made the style of the website slightly better in a way that reflects the project more.
    - Made a correction for the fetch code from the javascript login file to the html file where the script is at the bottom.

--- src/Web dashboard/login.js
+++ src/Web dashboard/login.js
@@ -12,23 +12,3 @@ function login() {
         document.getElementById("message").innerText = "Login failed";
     }
 }
-
-/**
- * This is where Javascript sends in the request for the login
-  */
-fetch("/login", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        "username": document.getElementById("username").value,
-        "password": document.getElementById("password").value
-    })
-})
-.then(response => response.json())
-.then(data => {
-    if(data.status === "success"){
-        alert("Login Successful");
-    }
-})
