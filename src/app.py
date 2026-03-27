1d83b7a7865ec1f2e1c772880cb6a149e762b3d1
Sange Doma Tamang <s.tamang3823@student.leedsbeckett.ac.uk>
2026-03-27 09:58:37 +0000

    CHECKPOINT - API development
    -Set up basic data storage structure for storing sensor data readings.
    -Modified air_data endpoint to store readings to store incoming data.
    -Created route to return stored readings.

--- src/app.py
+++ src/app.py
@@ -25,6 +25,7 @@ except ImportError:
     REAL_SENSOR = False
 
 app = Flask(__name__)
+data_store = []
 CORS(app) # CORS support in case the flask server is not accessible from the browser
 '''This is the server for the application.
 This server allows us to be able to add the functions for each
@@ -50,14 +51,19 @@ def air_data():
     data = sensor.read_data()
 
     if data is None:
-        return jsonify({
+        data = {
             "temperature": 22.5,
             "humidity": 55,
             "pressure": 1000,
             "gas": 120
-        })
-
+        }
+    else:
+        data_store.append(data)
     return jsonify(data)
+
+@app.route("/air_data/history")
+def air_data_history():
+    return jsonify(data_store)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=14473, debug=True)
